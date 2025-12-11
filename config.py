6acf03eb2e7b503aa088bce126d131e07725b46a
nobixnn <nobivc01@gmail.com>
2025-12-11 18:18:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_ID = 6435225
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7833972330:AAErp7yd_GYU9UyvE84tyfvFlb0KctRe9do")
+BOT_TOKEN = getenv("BOT_TOKEN", "7946219352:AAE2QqUt37x_Yl3e7ozTBMcsciOKm4Z4QOE")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", 7668520999))
+OWNER_ID = int(getenv("OWNER_ID", 7767163190)
 SUPPORT_GRP = "RIYA_chat_SUPPORT"
 UPDATE_CHNL = "riya_NETWORK"
 OWNER_USERNAME = "ll_hunter_lll"
